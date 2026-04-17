e86bab13ea3f25d1ffb58840745a09b1e753af1d
Ricardo Tanaka Terukina <rtanaka@talento.tls.edu.pe>
2026-04-16 19:33:57 -0500

    Added player

---
+++ Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra1261
+{
+    internal class Player : Character
+    {
+        public Player(string name, int health, int age) : base(name, health, age)
+        {
+        }
+    }
+}
